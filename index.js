37ca728fcd4c78de50f42125f1a99dba3fd2de92
Harish Krishnan <harishkrishnan1993@gmail.com>
2019-09-13 14:36:33 +0530

    Added basic html templates

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ const url = require('url');
 // console.log('will read file');
 
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -40,6 +44,17 @@ const server = http.createServer((req, res) => {
     res.end('This is OVERVIEW');
   } else if (pathName === '/product') {
     res.end('This is PRODUCT');
+  } else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
   }
 });
 
