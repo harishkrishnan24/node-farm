bec92301edf4a64b991d84c339299b5d6d91c318
Harish Krishnan <harishkrishnan1993@gmail.com>
2019-09-16 23:25:04 +0530

    Added templates and routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 // Blocking, Synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
@@ -35,20 +37,51 @@ const url = require('url');
 // SERVER
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
-  if (pathName === '/' || pathName === '/overview') {
-    res.end('This is OVERVIEW');
-  } else if (pathName === '/product') {
-    res.end('This is PRODUCT');
-  } else if (pathName === '/api') {
+  // Overview
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
+
+    // Product
+  } else if (pathname === '/product') {
+    const product = dataObj[query.id];
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // APi
+  } else if (pathname === '/api') {
     res.writeHead(200, {
       'Content-type': 'application/json'
     });
     res.end(data);
+
+    // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
